feb9c46629c7583cb38995df2762cf7ccd2d9c40
Harley Trung <harley@socialsci.com>
2015-03-23 16:07:46 +0700

    add current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_filter :fix_cas_session
   before_action :require_user
+  helper_method :current_user
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -18,11 +19,15 @@ class ApplicationController < ActionController::Base
     if session[:cas].nil? || session[:cas][:user].nil?
       render status: 401, text: "Redirecting to SSO..."
     else
-      @user ||= User.where(netid: session[:cas][:user], session: session[:cas].to_json).first_or_create!
+      @current_user ||= User.where(netid: session[:cas][:user], session: session[:cas].to_json).first_or_create!
     end
   end
 
   def skip_login?
     false
+  end
+
+  def current_user
+    @current_user ||= require_user
   end
 end
