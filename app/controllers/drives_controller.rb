faa92f842d4e7cc1d48bf4cfb1ed9ed200312b03
Harley Trung <harley@socialsci.com>
2015-03-23 17:25:06 +0700

    Edit Drive

---
+++ app/controllers/drives_controller.rb
@@ -0,0 +1,20 @@
+class DrivesController < ApplicationController
+  def edit
+    @drive = Drive.find params[:id]
+  end
+
+  def update
+    @drive = Drive.find params[:id]
+    if @drive.update_attributes drive_params
+      flash[:success] = "Changes saved."
+      redirect_to edit_drive_path(@drive)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def drive_params
+    params[:drive].permit!
+  end
+end
