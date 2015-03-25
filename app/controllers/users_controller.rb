6014738d97481d7c22519c76cc37bc58f19cdb81
Harley Trung <harley@socialsci.com>
2015-03-25 09:34:12 +0700

    let user update his profile. netid is readonly

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes user_params
+      flash[:success] = "Changes saved!"
+    else
+      flash[:error] = "Not all changes were saved"
+    end
+    redirect_to edit_user_path(:current)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:fname, :lname, :college, :class_year, :email, :lookup_by_email)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :drives, only: [:show] do
     resources :appointments
   end
